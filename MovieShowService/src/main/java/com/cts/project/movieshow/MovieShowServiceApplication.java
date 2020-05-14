c599151e267482170dec31272bf2b94b98dbb6be
ctsProject-star <Shivangi.Sharma3@cognizant.com>
2020-05-15 00:21:26 +0530

    commit

--- MovieShowService/src/main/java/com/cts/project/movieshow/MovieShowServiceApplication.java
+++ MovieShowService/src/main/java/com/cts/project/movieshow/MovieShowServiceApplication.java
@@ -1,17 +1,22 @@
 package com.cts.project.movieshow;
 
+import java.util.concurrent.Executor; 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.reactive.function.client.WebClient;
 //import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableAsync
 public class MovieShowServiceApplication {
 	
 //	@Bean     // Create a bean for restTemplate to call services   
@@ -20,6 +25,18 @@ public class MovieShowServiceApplication {
 //	{
 //	    return new RestTemplate();
 //	}
+	
+	@Bean
+	public Executor taskExecutor() 
+	{
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(200);
+		executor.setThreadNamePrefix("AysncServiceThreadLookup-");
+		executor.initialize();
+		return executor;
+	}
 	
 	@Bean
 	@LoadBalanced
@@ -33,14 +50,3 @@ public class MovieShowServiceApplication {
 	}
 
 }
-
-//@Configuration
-//class RestTemplateConfig {
-//	
-//	// Create a bean for restTemplate to call services
-//	@Bean
-//	@LoadBalanced		// Load balance between service instances running at different ports.
-//	public RestTemplate restTemplate() {
-//	    return new RestTemplate();
-//	}
-//}
--- MovieShowService/src/main/java/com/cts/project/movieshow/controller/MovieShowController.java
+++ MovieShowService/src/main/java/com/cts/project/movieshow/controller/MovieShowController.java
@@ -1,7 +1,8 @@
 package com.cts.project.movieshow.controller;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +25,15 @@ import com.cts.project.movieshow.entity.Theater;
 import com.cts.project.movieshow.errorHandling.CustomErrorType;
 import com.cts.project.movieshow.errorHandling.MovieShowNotFoundException;
 import com.cts.project.movieshow.repository.MovieShowRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 @RequestMapping("/movieshow")
 public class MovieShowController 
 {
-	  @Autowired
+
+	@Autowired
 	  private MovieShowRepository movieShowRepository;
 	 
 	  @Autowired
@@ -50,9 +55,9 @@ public class MovieShowController
 	               // You many decide to return HttpStatus.NOT_FOUND
 	            }
 	        }
-	        catch(RuntimeException e) 
+	        catch(Exception e) 
 			{ 
-				logger.error("RunTimeException is thrown... ");
+				logger.error("RunTimeException is thrown... ",e);
 				throw new RuntimeException(e);
 			}
 	        
@@ -60,35 +65,54 @@ public class MovieShowController
 	    }
       
       
-       @SuppressWarnings({ "unchecked", "rawtypes" })
+       @SuppressWarnings({ "unchecked", "rawtypes"})
 	   @RequestMapping(value = "/", method = RequestMethod.POST)
+       @HystrixCommand(fallbackMethod = "createFallBackMovieShow", 
+                        commandProperties = { 
+                        		@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                        		@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                        		@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                        		@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")}
+   		              )
        public ResponseEntity<?> createMovieShow(@RequestBody MovieShow movieShow, UriComponentsBuilder ucBuilder) throws IOException
        {
               logger.info("Creating new Movie Show : {}", movieShow);
-              Movie movie;
-              Theater theater;
+              CompletableFuture<Movie> movie = new CompletableFuture<Movie>();
+              CompletableFuture<Theater> theater = new CompletableFuture<Theater>();
               
               try 
               {
-            	  movie = webClientBuilder.build() // API call for movie service
-                          .get()
-                          .uri("http://movie-service/movie/" + movieShow.getMovieId())
-                          .retrieve().bodyToMono(Movie.class)
-                          .block();
-                  
-	        	 theater = webClientBuilder.build() // API call for theater service
-	                       .get()
-	                       .uri("http://theater-service/theater/" + movieShow.getTheaterId())
-	                       .retrieve().bodyToMono(Theater.class)
-	                       .block();
-                  
-               } 
+            	  movie = validateMovie(movieShow);
+            	              
+            	  theater = validateTheater(movieShow);
+            	  
+            	  if((movie == null) && (theater == null))
+            	  {
+            		  logger.error("Unable to create Movie Show : {} as given movie and theater does not exist", movieShow);
+      	              return new ResponseEntity(new CustomErrorType("404","Unable to create Movie Show with movie_Id: "+ movieShow.getMovieId()+" and theater Id: " + 
+      	                      movieShow.getTheaterId() + ", as the entered movie and theater does not exist."),HttpStatus.NOT_FOUND);
+            	  }
+            	  
+            	  if(movie == null)
+            	  {
+            		  logger.error("Unable to create Movie Show : {} as given movie does not exist", movieShow);
+    	              return new ResponseEntity(new CustomErrorType("404","Unable to create Movie Show with movie_Id: "+ movieShow.getMovieId() +
+    	            		                                         ", as the entered movie does not exist."),HttpStatus.NOT_FOUND);
+            	  }
+            	  
+            	  if(theater == null)
+            	  {
+            		  logger.error("Unable to create Movie Show : {} as given theater does not exist", movieShow);
+    	              return new ResponseEntity(new CustomErrorType("404","Unable to create Movie Show with theater_Id: "+ movieShow.getTheaterId() +
+    	            		                                         ", as the entered theater does not exist."),HttpStatus.NOT_FOUND);
+            	  }          		
+
+              } 
     	          
-              catch(RuntimeException e)
+              catch(Exception e)
     	      {
-    	              logger.error("Unable to create Movie Show : {} as given movie or theater does not exist", movieShow);
-    	              return new ResponseEntity(new CustomErrorType("Unable to create Movie Show with movie_Id: "+ movieShow.getMovieId()+" and theater Id: " + 
-    	                      movieShow.getTheaterId() + ", as the entered movie or theater does not exist."),HttpStatus.NOT_FOUND);
+            	  logger.error("RunTimeException is thrown... ",e);
+  				  throw new RuntimeException(e);
     	      }
     	          	          
     	      movieShowRepository.save(movieShow);
@@ -98,10 +122,67 @@ public class MovieShowController
 	          headers.setLocation(ucBuilder.path("/movieshow/{movieshowId}").buildAndExpand(movieShow.getId()).toUri()); 
 	          return new ResponseEntity<String>(headers, HttpStatus.CREATED);
        }
+       
+       @Async                    
+       private CompletableFuture<Theater> validateTheater(MovieShow movieShow) 
+       {
+    	   Theater theater;
+    	   try 
+    	   {
+    		   logger.info("getTheaterId() starts...");
+    		   theater = webClientBuilder.build() // API call for theater service
+                        .get()
+                        .uri("http://theater-service/theater/" + movieShow.getTheaterId())
+                        .retrieve().bodyToMono(Theater.class)
+                        .block();
+    		   
+    		   logger.info("Theater Id:"+ theater.getId());
+          	   logger.info("getTheaterId() completed...");
+    	   }
+    	   catch(Exception e)
+    	   {
+    		   return null;
+    	   }
+	
+		return CompletableFuture.completedFuture(theater);
+	   }
+
+       @Async
+	   public CompletableFuture<Movie> validateMovie(@RequestBody MovieShow movieShow) throws IOException
+       {
+    	   Movie movie;
+    	   try 
+    	   {
+    		  logger.info("getMovieId() starts...");
+    	      movie = webClientBuilder.build() // API call for movie service
+                           .get()
+                           .uri("http://movie-service/movie/" + movieShow.getMovieId())
+                           .retrieve().bodyToMono(Movie.class)
+                           .block();
+    	      
+    	      logger.info("Movie Id:"+ movie.getId());
+         	  logger.info("getMovieId() completed...");
+         	  
+    	   }
+    	   catch(Exception e)
+    	   {
+    		  return null;
+    	   }
+
+		   return CompletableFuture.completedFuture(movie);
+    	   
+       }
+       
+       @SuppressWarnings({ "unchecked", "rawtypes" })
+	   public ResponseEntity<?> createFallBackMovieShow(@RequestBody MovieShow movieShow, UriComponentsBuilder ucBuilder)
+       {
+		   return new ResponseEntity(new CustomErrorType("500","Unable to create Movie Show {}: "+ movieShow +
+				                     ", as the circuit between the services has broken."),HttpStatus.INTERNAL_SERVER_ERROR);
+       }
    
 
 	   @SuppressWarnings({ "unchecked", "rawtypes" })
-	@RequestMapping(value = "/{movieshowId}", method = RequestMethod.PUT)
+	   @RequestMapping(value = "/{movieshowId}", method = RequestMethod.PUT)
        public ResponseEntity<MovieShow> updateMovieShow(@PathVariable("movieshowId") Integer movieshowId, @RequestBody MovieShow movieShowRequest) throws IOException
        {
 		    logger.info("Updating Movie Show with id {}", movieshowId);
@@ -113,15 +194,15 @@ public class MovieShowController
 	    		 if (movieshow == null)
 				 {
 					 logger.error("Movie Show with id {} not found.", movieshowId);
-					 return new ResponseEntity(new CustomErrorType("Unable to find Movie Show with id: " +  movieshowId + " ,does not exist."),HttpStatus.NOT_FOUND);
+					 return new ResponseEntity(new CustomErrorType("404","Unable to find Movie Show with id: " +  movieshowId + " ,does not exist."),HttpStatus.NOT_FOUND);
 				 }
 
 	    		 movieshow.setsTime(movieShowRequest.getsTime());
 	    		
 	    	}
-	    	catch(RuntimeException e)
+	    	catch(Exception e)
 	    	{
-	    		logger.error("RunTimeException is thrown... ");
+	    		logger.error("RunTimeException is thrown... ",e);
 				throw new RuntimeException(e);
 	    	}
 
